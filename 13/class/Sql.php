7a40566870d4645fb29c2b58fccc90e9b535baf4
Clemilton Pereira <clemilton.ufam@gmail.com>
2018-04-11 22:24:17 -0400

    alteracao pdo

---
+++ 13/class/Sql.php
@@ -0,0 +1,48 @@
+<?php
+
+class Sql extends PDO{
+
+	private $conn;
+
+	public function __construct(){
+
+		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","root");
+
+	}
+
+	private function setParams($statement,$params = array()){
+		foreach ($params as $key => $value) {
+			$this->setParam($statement,$key,$value);
+		}
+	}
+
+	private function setParam($statement, $key,$value){
+		$statement->bindParam($key,$value);
+	}
+
+	public function query($rawquery,$params = array()){
+		$stmt = $this->conn->prepare($rawquery);
+
+		$this->setParams($stmt,$params);
+		
+		$stmt->execute();
+
+
+		return $stmt;
+		
+	}
+
+	public function select($rawquery,$params = array()):array
+	{
+		
+		$stmt = $this->query($rawquery,$params);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		
+	}
+
+
+}
+
+
+?>
